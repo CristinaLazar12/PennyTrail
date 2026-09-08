ef9e3623dbb56e3ddc701c322d280b8eb4d42396
CristinaLazar12 <cristinalazar122@gmail.com>
2026-09-08 16:58:23 -0700

    Test valid and invalid expense creation

--- test/controllers/expenses_controller_test.rb
+++ test/controllers/expenses_controller_test.rb
@@ -15,4 +15,43 @@ class ExpensesControllerTest < ActionDispatch::IntegrationTest
     assert_select "h2", text: "Lunch at cafe"
     assert_select "p", text: "RON 45.50"
   end
+
+  test "creates an expense with valid attributes" do
+    assert_difference("Expense.count", 1) do
+      post expenses_url, params: {
+        expense: {
+          description: "Dinner",
+          amount: "45.50",
+          spent_on: Date.current,
+          category: "Food"
+        }
+      }
+    end
+
+    assert_redirected_to expenses_url
+  end
+
+  test "does not create an expense with invalid attributes" do
+    assert_no_difference("Expense.count") do
+      post expenses_url, params: {
+        expense: {
+          description: "Dinner",
+          amount: "0",
+          spent_on: Date.current,
+          category: "Food"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "li", text: "Amount must be greater than 0"
+    assert_select 'input[name="expense[description]"][value="Dinner"]'
+    assert_select 'input[name="expense[category]"][value="Food"]'
+    assert_select 'input[name="expense[amount]"]' do |inputs|
+      assert_equal BigDecimal("0"), BigDecimal(inputs.first["value"])
+    end
+    assert_select 'input[name="expense[spent_on]"]' do |inputs|
+      assert_equal Date.current.to_s, inputs.first["value"]
+    end
+  end
 end
